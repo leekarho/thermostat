4a9f2a73f3eb86fabeb35de9a86893214a3827f5
leekarho <k-h_lee@hotmail.co.uk>
2018-09-05 16:43:42 +0100

    added energy usage

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -22,7 +22,43 @@ describe('thermostat', function(){
   });
 
   it('has a minimum temp of 10 degrees', function() {
-    thermostat.temp = 10
+    thermostat.temp = 10;
     expect(function(){ thermostat.decreaseTemp(); }).toThrowError('this is already the min temp');
   });
+
+  it('has a max temp of 25 if power saving mode is on', function(){
+    thermostat.temp = 25;
+    spyOn(thermostat,'isPowerSaving').and.returnValue(true);
+    expect(function(){ thermostat.increaseTemp(); }).toThrowError('power saving on 25 degrees is the max temp');
+  });
+
+  it('has a max temp of 32 degrees if power saving mode is off', function(){
+    thermostat.temp = 32;
+  //  spyOn(thermostat,'isPowerSaving').and.returnValue(false);
+    expect(function(){ thermostat.increaseTemp(); }).toThrowError('this is the max temp') ;
+  });
+
+  it('has a temp of 20 degrees if reset option chosen', function(){
+    thermostat.reset();
+    expect(thermostat.temp).toEqual(20);
+  });
+
+  describe('energy usage', function(){
+    it('has current energy usage of low-usage', function(){
+      thermostat.temp = 15
+      expect(thermostat.energyUsage(thermostat.temp)).toEqual('low-usage');
+    });
+
+    it('has current energy usage of medium-usage', function(){
+      thermostat.temp = 21
+      expect(thermostat.energyUsage(thermostat.temp)).toEqual('medium-usage');
+    });
+
+    it('has current energy usage of high-usage', function(){
+      thermostat.temp = 31
+      expect(thermostat.energyUsage(thermostat.temp)).toEqual('high-usage');
+    });
+  });
+
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,12 +1,21 @@
 function Thermostat(){
   this.temp = DEFAULT_TEMP;
-
+  this.isPowerSaving = true;
 }
 
 const DEFAULT_TEMP = 20;
 const MIN_TEMP = 10;
+const MAX_TEMP = 32;
+const MAX_POWER_SAVING_TEMP = 25;
+
 
 Thermostat.prototype.increaseTemp = function () {
+  if((this.isPowerSaving) && (this.temp === MAX_POWER_SAVING_TEMP)) {
+    throw new Error('power saving on 25 degrees is the max temp');
+  }
+  else if(this.temp === MAX_TEMP) {
+    throw new Error('this is the max temp');
+  }
   return this.temp += 1;
 };
 
@@ -15,4 +24,22 @@ Thermostat.prototype.decreaseTemp = function () {
     throw new Error('this is already the min temp');
   }
   return this.temp -= 1;
+};
+
+// Thermostat.prototype.powerSavingOff = function (){
+//   return this.isPowerSaving = false
+// };
+
+Thermostat.prototype.reset = function() {
+  return this.temp = DEFAULT_TEMP;
+};
+
+Thermostat.prototype.energyUsage = function(temp) {
+  if (temp < 18) {
+    return 'low-usage';
+  } else if (temp < 25){
+    return 'medium-usage';
+  } else {
+    return 'high-usage';
+  }
 };
